afe86c6cedca694ca783b53f1d365c07aa75418d
sobirova79 <sobirovabdulaziz20@gmail.com>
2023-09-06 12:52:52 +0500

    todo list

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useReducer, useState } from "react";
+import React from "react";
+import TodoList from "./TodoList";
+import { Context } from "./context";
+import reducer from "./reducer";
 
-function App() {
+export default function App() {
+  const [state, dispatch] = useReducer(
+    reducer,
+    JSON.parse(localStorage.getItem("todos"))
+  );
+  const [todoTitle, setTodoTitle] = useState("");
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(state));
+  }, [state]);
+
+  const addTodo = (event) => {
+    if (event.key === "Enter") {
+      dispatch({
+        type: "add",
+        payload: todoTitle,
+      });
+
+      setTodoTitle("");
+    }
+  };
+
+  // const removeTodo = (id) => {
+  //   setTodos(
+  //     todos.filter((todo) => {
+  //       return todo.id !== id;
+  //     })
+  //   );
+  // };
+  // const toggleTodo = (id) => {
+  //   setTodos(
+  //     todos.map((todo) => {
+  //       if (todo.id === id) {
+  //         todo.completed = !todo.completed;
+  //       }
+  //       return todo;
+  //     })
+  //   );
+  // };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Context.Provider
+      value={{
+        dispatch,
+      }}
+    >
+      <div className="container">
+        <h1>Todo app </h1>
+
+        <div className="input-field">
+          <input
+            className="input-field_input"
+            type="text"
+            value={todoTitle}
+            onChange={(event) => setTodoTitle(event.target.value)}
+            onKeyPress={addTodo}
+            placeholder="Todo name"
+          />
+        </div>
+
+        <TodoList todos={state} />
+      </div>
+    </Context.Provider>
   );
 }
-
-export default App;
